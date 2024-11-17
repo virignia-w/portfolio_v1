3673321bd46833f3cebce63590e9b3b2091dffb7
Virginia <ning93315@gmail.com>
2024-11-17 20:14:23 +0000

    adding css

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,11 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="./style.css" /> 
+
 
         <title>Virginia W</title>
-        <link rel="stylesheet" href="./style.css" />
+        
       </head>
       <body>{children}</body>
     </html>
