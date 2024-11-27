f1a4d83d2264b430f006b56e95cc5987ca8d889f
Virginia <ning93315@gmail.com>
2024-11-27 05:05:38 +0000

    add home page style

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,11 +7,40 @@ export default function Page() {
    return <>
       <Header />
       <div className="homeBodyCont">
+
+         <div className="greetingComtainer">
+            <div className="sayHi">
+               <div className="greetingHey">
+                  <img src="images/Hey.png" />
+               </div>
+               <div className="greetingThere">
+                  <img src="images/there.png" />
+               </div>
+            </div>
+            <div className="whoIam">
+               <div className="greetingIm">
+                  <img src="images/Im.png" />
+               </div>
+               <div className="greetingNing">
+                  <img src="images/NING.png" />
+               </div>
+               <div className="greetingWang">
+                  <img src="images/Wang.png" />
+               </div>
+            </div>
+            <div className="also">
+               <div className="greetingAlso">
+                  <img src="images/also.png" />
+               </div>
+               <div className="greetingVir">
+                  <img src="images/Vir.png" />
+               </div>
+            </div>
+         </div>
+
+
          <div className="homeBGContainer">
             <Image className="homeBG" fill src={homeBG} objectFit={'contain'} alt="home page background" />
-         </div>
-         <div className="GreetingComtainer">
-            <img className="greetingImg" src="./public/Hey.png" />
          </div>
       </div>
 
